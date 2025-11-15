3bf737a5062aa65b2ef79b3c8879d3918d1f5501
Kaique Lopes <41844709+Kaique-Lopes@users.noreply.github.com>
2025-11-14 21:28:29 -0300

    create project

---
+++ OnboardingApp/OnboardingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OnboardingApp
+//
+//  Created by Kaique Lopes on 14/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
